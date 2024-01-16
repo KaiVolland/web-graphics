024ec6c042515c07bcd30fefd0e02b8cbda9e8c3
Kai Volland <volland@terrestris.de>
2024-01-16 16:58:34 +0100

    add svelte-spa-router

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,40 @@
 <script lang="ts">
-  import ShaderArt from "./pages/shaderArt/ShaderArt.svelte";
+  import {link} from 'svelte-spa-router'
+  import Router from 'svelte-spa-router';
+  import Home from './routes/Home.svelte';
+  import ShaderArt from './routes/shaderart/ShaderArt.svelte';
+
+  const routes = {
+    '/': Home,
+    // Exact path
+    '/shaderart': ShaderArt,
+  };
 </script>
 
-<main>
-  <ShaderArt />
-</main>
+<div class="content">
+  <header>
+    <nav>
+      <a href="/home" use:link>Home</a>
+      <a href="/shaderart" use:link>Shader Art</a>
+    </nav>
+  </header>
+  <main>
+    <Router {routes} />
+  </main>
+</div>
 
-<style>
+<style lang="less">
+  div.content {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+
+    header {
+      height: 3em;
+    }
+    main {
+      flex: 1;
+      overflow: auto;
+    }
+  }
 </style>
