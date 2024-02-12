98b82ed059e260ac2460219980eb23455ac025f2
Kai Volland <volland@terrestris.de>
2024-02-12 17:00:11 +0100

    apply resizehandling for ShaderArt

--- src/routes/shaderart/ShaderArt.svelte
+++ src/routes/shaderart/ShaderArt.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-  import vertexShaderSource from "../../shaderArt/main.vert.js";
-  import fragmentShaderSource from "../../shaderArt/main.frag.js";
-  import { density } from "../../store/shaderArt/density.js";
-  import { speedFactor } from "../../store/shaderArt/speedFactor.js";
-  import { onMount } from "svelte";
-  import SpeedSlider from "../../lib/shaderArt/SpeedSlider.svelte";
-  import DensitySlider from "../../lib/shaderArt/DensitySlider.svelte";
+  import vertexShaderSource from '../../shaderArt/main.vert.js';
+  import fragmentShaderSource from '../../shaderArt/main.frag.js';
+  import { density } from '../../store/shaderArt/density.js';
+  import { speedFactor } from '../../store/shaderArt/speedFactor.js';
+  import { onMount } from 'svelte';
+  import SpeedSlider from '../../lib/shaderArt/SpeedSlider.svelte';
+  import DensitySlider from '../../lib/shaderArt/DensitySlider.svelte';
 
   let canvas: HTMLCanvasElement;
 
   onMount(() => {
-    const gl = canvas.getContext("webgl2");
+    const gl = canvas.getContext('webgl2');
 
     if (!gl) {
       return;
@@ -48,13 +48,13 @@
       -1, -1, 1, -1, -1, 1, 1, -1, -1, 1, 1, 1,
     ]);
 
-    const uPositionLoc = gl.getUniformLocation(program!, "uResolution");
+    const uPositionLoc = gl.getUniformLocation(program!, 'uResolution');
     gl.uniform2f(uPositionLoc, 1024, 1024);
 
-    const uTimeLoc = gl.getUniformLocation(program!, "uTime");
+    const uTimeLoc = gl.getUniformLocation(program!, 'uTime');
     gl.uniform1f(uTimeLoc, 0);
 
-    const uDensityLoc = gl.getUniformLocation(program!, "uDensity");
+    const uDensityLoc = gl.getUniformLocation(program!, 'uDensity');
     gl.uniform1f(uDensityLoc, 15.0);
 
     const buffer = gl.createBuffer();
@@ -66,6 +66,17 @@
     gl.vertexAttribPointer(aPositionLoc, 2, gl.FLOAT, false, 2 * 4, 0);
 
     const draw = (time = 0) => {
+      // Check if the canvas is not the same size.
+      const needResize =
+        canvas.width !== canvas.clientWidth ||
+        canvas.height !== canvas.clientHeight;
+      if (needResize) {
+        // Make the canvas the same size
+        canvas.width = canvas.clientWidth;
+        canvas.height = canvas.clientHeight;
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+      }
+
       gl.clearColor(0, 0, 0, 1.0);
       gl.clear(gl.COLOR_BUFFER_BIT);
 
@@ -82,16 +93,33 @@
   });
 </script>
 
-<div class="toolbar">
-  <label for="speed-slider">Speed</label>
-  <SpeedSlider id="speed-slider" />
-  <label for="speed-slider">Density</label>
-  <DensitySlider id="density-slider" />
-</div>
-<canvas bind:this={canvas} width="1024" height="1024"></canvas>
-<div>
-  <a href="https://www.youtube.com/watch?v=f4s1h2YETNY&t=583s">Shader code by kishimisu</a>
+<div class="shaderArt">
+  <div class="toolbar">
+    <label for="speed-slider">Speed</label>
+    <SpeedSlider id="speed-slider" />
+    <label for="speed-slider">Density</label>
+    <DensitySlider id="density-slider" />
+  </div>
+  <canvas bind:this={canvas} ></canvas>
+  <div>
+    <a href="https://www.youtube.com/watch?v=f4s1h2YETNY&t=583s"
+      >Shader code by kishimisu</a
+    >
+  </div>
 </div>
 
-<style>
+<style lang="less">
+  .shaderArt {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+
+    canvas {
+      flex: 1;
+      overflow: hidden;
+      border: 1px solid const(--accent);
+      background-color: #f7f5ed;
+    }
+  }
 </style>
